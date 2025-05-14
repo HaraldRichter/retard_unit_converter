70cfa24ce10dd8162497c7c4f7714a2679684129
RichterH <h.richter@elo.com>
2025-05-14 15:15:31 +0200

    Conversion Container können nun flexibel  generiert werden, statt dass sie hardcoded sind

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
+const addButton = document.getElementById("add-btn");
+const conversionContainerGroup = document.getElementById(
+    "conversion-container-group"
+);
 
-const conversionContainers = document.querySelectorAll(".conversion-container");
+// Erstellt eine Liste aller Retard Units, für die ein Conversion-Container gebaut werden soll.
+// Die Liste ist dann im Browser zu speichern. Standard-Anfangseinstellung sind Feet, Inch, Pounds.
+let retardUnitsList = ["feet", "inch", "pounds"];
+initializeContainers(retardUnitsList);
 
+function initializeContainers(retardUnitsList) {
+    let containers = "";
+    retardUnitsList.forEach((retardUnit) => {
+        containers += `
+            <div class="conversion-container" id="${retardUnit}">
+                <label for="${retardUnit}">${retardUnit}:</label>
+                <input type="number" class="retard" id="input-${retardUnit}" />
+                <label for="${retardUnit}">to ${getNonRetardedUnit(retardUnit)}</label>
+                <p class="non-retard" id="output-${retardUnit}"></p>
+            </div>
+        `;
+    });
+    conversionContainerGroup.innerHTML = containers;
+}
+
+// Erstellt ein Array mit allen Conversion-Containern:
+let conversionContainers = document.querySelectorAll(".conversion-container");
+
+// Definiert anschließend das Verhalten für alle Conversion-Containers:
 conversionContainers.forEach((container) => {
     console.log("Container-id: " + container.id);
     const retardUnit = container.id;
@@ -18,13 +44,22 @@ conversionContainers.forEach((container) => {
     });
 });
 
+// TODO: Füge einen Conversion-Container mit bestimmten Parametern dem Container-Array hinzu
+// function addConversionContainer(retardUnit) {
 
+// }
+
+// TODO: Lösche einen bestimmten Conversion-Container aus dem Array
+
+// Innere Funktion(en):
 function convertToNonRetard(retardUnit, value) {
     switch (retardUnit) {
+        case "feet":
+            return parseFloat(value * 0.3048).toFixed(2);
         case "inch":
-            return value * 2.54;
+            return parseFloat(value * 2.54).toFixed(2);
         case "pounds":
-            return value * 0.45;
+            return parseFloat(value * 0.45).toFixed(2);
     }
 }
 
